892bb75b87b9e2f9234060a55a1e8c0a5c5230dc
moayed-mustafa <moayed.abrida@yahoo.com>
2020-05-31 18:16:00 +0400

    initial commit....again

---
+++ app.py
@@ -0,0 +1,9 @@
+"""Flask app for Cupcakes"""
+from flask import Flask, request, jsonify
+from models import Cupcake, db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+connect_db(app)
---
+++ models.py
@@ -0,0 +1,30 @@
+"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+
+
+class Cupcake(db.Model):
+    """ model to meror the cupcake table """
+
+    tablename =" __cupcake__"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default="https://images.unsplash.com/photo-1583625494781-8f80e36bd727?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80")
+
+
+    def serialize(self):
+        return {
+            'flavor': self.flavor,
+            'size': self.size,
+            'rating': self.rating,
+            'image': self.image
+        }
